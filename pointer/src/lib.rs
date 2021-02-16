50ca1dc9ed2e496779485422a10551ca2d69446f
Alex Sarapulov <1molyee@gmail.com>
2021-02-16 19:15:05 +0700

    [pointer] update usage of mem::size_of

--- pointer/src/lib.rs
+++ pointer/src/lib.rs
@@ -1,10 +1,10 @@
 use core::ptr::{write_unaligned, copy_nonoverlapping};
-use core::mem;
+use core::mem::size_of;
 
 #[inline]
 pub unsafe fn write_next<T, U>(dst: *mut T, src: T) -> *mut U {
     write_unaligned(dst, src);
-    let size = mem::size_of::<T>() as isize;
+    let size = size_of::<T>() as isize;
     dst.offset(size) as *mut U
 }
 
@@ -20,6 +20,6 @@ pub unsafe fn try_write_next<T, U>(dst: *mut T, src: Option<T>) -> *mut U {
 #[inline]
 pub unsafe fn copy_next<T, U>(src: *const T, dst: *mut T, count: usize) -> *mut U {
     copy_nonoverlapping(src, dst, count);
-    let size = (mem::size_of::<T>() * count) as isize;
+    let size = (size_of::<T>() * count) as isize;
     dst.offset(size) as *mut U
 }
